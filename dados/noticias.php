8b45316c6478066d39113384034331d644509b0b
MateusLinoo <136358937+MateusLinoo@users.noreply.github.com>
2025-04-12 22:29:24 -0300

    Update noticias.php

---
+++ dados/noticias.php
@@ -0,0 +1,18 @@
+<?php
+$noticias = [
+    [
+        "id" => 1,
+        "titulo" => "Nova política ambiental em Curitiba",
+        "categoria" => "Meio Ambiente",
+        "descricao" => "A prefeitura de Curitiba lançou novas diretrizes para proteção de áreas verdes.",
+        "imagem" => "imagens/meio_ambiente.jpg"
+    ],
+    [
+        "id" => 2,
+        "titulo" => "Festival gastronômico reúne chefs renomados",
+        "categoria" => "Gastronomia",
+        "descricao" => "Evento traz pratos exclusivos e experiências culinárias únicas.",
+        "imagem" => "imagens/gastronomia.jpg"
+    ]
+];
+?>
